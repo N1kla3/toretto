531400c8dc5305378c6d5ac5d2922b820cc850e6
kolya <kolya.vladimirsky@gmail.com>
2021-07-29 00:39:31 +0300

    camera added

---
+++ Source/Dom/DomCamera.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
+#include "DomCamera.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UDomCamera : public UCameraComponent
+{
+	GENERATED_BODY()
+	
+};
